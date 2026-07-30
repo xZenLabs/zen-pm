806f5dd34a3dcb0dc876ce261e0df90ec484070b
Anthony Gress <the.techx.guy@gmail.com>
2026-07-29 22:24:40 -0400

    update log

--- frontend/koreader/zenpm.koplugin/config/changelog.lua
+++ frontend/koreader/zenpm.koplugin/config/changelog.lua
@@ -30,6 +30,7 @@ return {
     ["1.2.0"] = {
         "Migrate from HTTP server to UDS for all systems",
         "Add non-touch support",
+        "Add ignored updates",
         "Remove confirmation for enable/disable",
         "Fix false unknown plugin matching",
         "Fix duplicate unknown and known entries for prev installed items",
